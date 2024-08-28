a40c7e36a939369545b9f49e4baad134e0ce8b47
luuka <Luukas.Rannila@metropolia.fi>
2024-08-28 13:02:07 +0300

    dog tasks

--- moduuli4/1.py
+++ moduuli4/1.py
@@ -1,2 +1,9 @@
 import math
+numero = int(1000)
+while (numero>1):
+    numero = numero - 1
+    if (numero%3==0):
+        print(numero)
 
+    elif (numero==0):
+        break
---
+++ moduuli4/2.py
@@ -0,0 +1,8 @@
+
+
+numba = int(input ("Anna numba: "))
+while numba>= 0:
+    print (numba * 2.54)
+    numba = (int(input("Anna numba: ")))
+
+print(numba)
