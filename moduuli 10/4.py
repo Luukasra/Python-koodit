0a814cb24d1ede793a12f095f6146a2719746ede
luuka <Luukas.Rannila@metropolia.fi>
2024-10-25 13:19:34 +0300

    dog tasks

--- moduuli 10/4.py
+++ moduuli 10/4.py
@@ -23,6 +23,33 @@ class Auto:
     def kulje(self, aika):
         self.kuljettu_km = aika * self.nopeus_rn + self.kuljettu_km
 
+class Kilpailu:
+    def __init__(self, nimi, pituuskm, autot):
+        self.nimi = nimi
+        self.pituuskm = pituuskm
+        self.autot = autot
+
+
+    def tunti_kuluu(self):
+        for Auto in self.autot:
+            nopeusmuutos = random.randint(-10, 15)
+            Auto.kiihdytä(nopeusmuutos)
+            Auto.kulje(1)
+
+    def tulosta_tilanne(self):
+        print(f"{'rekkari':<10} {'max sped':<10} {'Nopeusrn':<10} {'kilsat':<10}")
+        for auto in self.autot:
+            print(f"{auto.rekisteri:<10} {auto.huippunopeus:<10} {auto.nopeus_rn:<10} {auto.kuljettu_km:<10f} km")
+
+    def kilpailu_ohi(self):
+
+        for auto in lista:
+
+            if auto.kuljettu_km >= self.pituuskm:
+                return True
+
+        return False
+
 lista = []
 
 for i in range(1, 11):
@@ -31,18 +58,17 @@ for i in range(1, 11):
     lista.append(Auto("ABC-"+str(i),huippunopeus))
 
 tosi = True
+
+kisa = Kilpailu("Suuri romuralli", 8000, lista)
+
+tunnit = 0
 while tosi:
+    kisa.tunti_kuluu()
+    tunnit = tunnit + 1
+    if kisa.kilpailu_ohi() == True:
+        print("ohikisa yippyy")
+        kisa.tulosta_tilanne()
 
-    for auto in lista:
-        nopeusmuutos = random.randint(-10, 15)
-        auto.kiihdytä(nopeusmuutos)
-        auto.kulje(1)
-        if auto.kuljettu_km >= 10000:
-            tosi = False
-            break
-
-
-print(f"{'rekkari':<10} {'max sped':<10} {'Nopeusrn':<10} {'kilsat':<10}")
-
-for auto in lista:
-    print(f"{auto.rekisteri:<10} {auto.huippunopeus:<10} {auto.nopeus_rn:<10} {auto.kuljettu_km:<10f} km")
+        break
+    if tunnit % 10 == 0:
+        kisa.tulosta_tilanne()
